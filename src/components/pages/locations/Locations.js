c79ef04e67de6c7f4be599d9f963c08e46aaa067
gilbu <gilbertbuix57@gmail.com>
2020-12-18 17:57:18 -0800

    add phone and fax icons

--- src/components/pages/locations/Locations.js
+++ src/components/pages/locations/Locations.js
@@ -13,8 +13,8 @@ function Locations() {
               <a href={img.placeID} target= "_blank"> <img src={img.image} alt={img.name}/></a>
               <div>{img.name}</div>
               <div>{img.address}</div>
-              <div>Phone: {img.phone}</div>
-              <div>Fax: {img.fax}</div>
+              <div><i class="fa">&#xf095;</i> {img.phone}</div>
+              <div><i class="fa">&#xf1ac;</i> {img.fax}</div>
             </div>
           </div>
         </div>
