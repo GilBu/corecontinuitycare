dd482663f4dc197f56ebc70699f55f543d40d9c4
gilbu <gilbertbuix57@gmail.com>
2020-12-20 16:05:40 -0800

    aline profile pictures vertically

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -18,29 +18,27 @@ function About() {
   }
   return (
     <React.Fragment>
-      <h1>About</h1>
-      <p className="about">We endeavor to promote and improve the health of individuals in the
-      post acute setting through evidence based medical treatment and
-      prevention strategies, effective use of data,  advocacy, and
-      collaboration.
+      <div className= "about-container">
+        <h1>About</h1>
+        <p className="about">We endeavor to promote and improve the health of individuals in the
+        post acute setting through evidence based medical treatment and
+        prevention strategies, effective use of data,  advocacy, and
+        collaboration.
 
-      Our goal is to offer consistent quality care that set the standards
-      in skilled nursing facility communities and exceed patients' expectations
-       while delivered in a caring, convenient, cost-effective and accessible
-       manner. This is exemplified through our belief of creating a culture
-       surrounding the core continuity of care for all of our patients,
-       creating lasting relationships with partners, and building strong
-       connections within the community.</p>
-       <h1>Meet the Team</h1>
-       <div className="middlethis" dir='rtl'>
-        {produceImageWidgets()}
-       </div>
+        Our goal is to offer consistent quality care that set the standards
+        in skilled nursing facility communities and exceed patients' expectations
+        while delivered in a caring, convenient, cost-effective and accessible
+        manner. This is exemplified through our belief of creating a culture
+        surrounding the core continuity of care for all of our patients,
+        creating lasting relationships with partners, and building strong
+        connections within the community.</p>
+        <h1>Meet the Team</h1>
+        <div className="middlethis" dir='rtl'>
+          {produceImageWidgets()}
+        </div>
+      </div>
     </React.Fragment>
   )
-}
-
-const testStyle = {
-  width: '50px'
 }
 
 export default About;
