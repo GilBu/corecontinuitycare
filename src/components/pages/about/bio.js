7d6f1ad162d2aa96fb012dadff09b294782c783d
gilbu <gilbertbuix57@gmail.com>
2020-12-22 20:08:37 -0800

    add bio

---
+++ src/components/pages/about/bio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+function Bio() {
+  return (
+    <React.Fragment>
+      <div className= "bio-container">
+        <h1>Bio</h1>
+        <p className="bio"></p>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Bio;
