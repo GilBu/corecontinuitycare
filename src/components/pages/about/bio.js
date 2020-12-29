91d76b5118f45577e712df4f93a574ef531ef847
gilbu <gilbertbuix57@gmail.com>
2020-12-28 19:32:23 -0800

    add constants for components

--- src/components/pages/about/bio.js
+++ src/components/pages/about/bio.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import './styles.css'
 
-function Bio() {
-  return (
-      <div
-        {...{
-          className: `accordion-item, ${opened && 'accordion-item--opened'}`,
-          onClick: () => { this.setState({ opened: !opened }) }
-        }}
-      >
-        <div {...{ className: 'accordion-item__line' }}>
-          <h3 {...{ className: 'accordion-item__title' }}>
-            {title}
-          </h3>
-        </div>
-          <div {...{ className: 'accordion-item__inner' }}>
-            <div {...{ className: 'accordion-item__content' }}>
-              <p {...{ className: 'accordion-item__paragraph' }}>
-                {bio}
-              </p>
-            </div>
+class Bio extends React.Component {
+  state = {
+    opened: false
+  }
+
+  render () {
+    const {
+      props: {
+        bio,
+        title
+      },
+      state: {
+        opened
+      }
+    } = this
+
+    return (
+        <div
+          {...{
+            className: `accordion-item, ${opened && 'accordion-item--opened'}`,
+            onClick: () => { this.setState({ opened: !opened }) }
+          }}
+        >
+          <div {...{ className: 'accordion-item__line' }}>
+            <h3 {...{ className: 'accordion-item__title' }}>
+              {title}
+            </h3>
           </div>
-      </div>
-    )
+            <div {...{ className: 'accordion-item__inner' }}>
+              <div {...{ className: 'accordion-item__content' }}>
+                <p {...{ className: 'accordion-item__paragraph' }}>
+                  {bio}
+                </p>
+              </div>
+            </div>
+        </div>
+      )
+  }
 }
 
 export default Bio;
