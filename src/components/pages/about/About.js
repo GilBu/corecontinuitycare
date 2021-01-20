38659b7e83d10ca151f19187312a7e8c66f366c7
gilbu <gilbertbuix57@gmail.com>
2021-01-19 23:09:03 -0800

    change about from function to class

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -4,15 +4,15 @@ import Bio from './bio.js'
 import './styles.css'
 import ContactUs from '../contactUs/ContactUs';
 
-function About() {
-  let selected = 0
+class About extends React.Component {
+  state ={selected: 0}
 
-  const onClickBio = (key) => {
-    selected = key;
-  }
+  // onClickBio = (key) => {
+  //   selected = key;
+  // }
 
-  const bio = (selected) => {
-    let selectedBio = imagesList[selected]
+  bio () {
+    let selectedBio = imagesList[this.state.selected]
     return (
       <div>
         <div>
@@ -29,11 +29,11 @@ function About() {
     )
   }
 
-  const produceImageWidgets = () => {
+  produceImageWidgets () {
     return imagesList.map((img, idx) => {
       return (
         <div className="responsive" key={idx.toString()} >
-          <div className="gallery" onClick={() => onClickBio(idx)}>
+          <div className="gallery" onClick={() => this.setState({selected: idx})}>
             <img src={img.image} alt={img.name}/>
             <div>{img.name}</div>
             <div dir='ltr'>{img.title}</div>
@@ -42,33 +42,36 @@ function About() {
       )
     });
   }
-  return (
-    <React.Fragment>
-      <div className= "about-container">
-        <h1>About</h1>
-        <p className="about">We endeavor to promote and improve the health of individuals in the
-        post acute setting through evidence based medical treatment and
-        prevention strategies, effective use of data,  advocacy, and
-        collaboration.
 
-        Our goal is to offer consistent quality care that set the standards
-        in skilled nursing facility communities and exceed patients' expectations
-        while delivered in a caring, convenient, cost-effective and accessible
-        manner. This is exemplified through our belief of creating a culture
-        surrounding the core continuity of care for all of our patients,
-        creating lasting relationships with partners, and building strong
-        connections within the community.</p>
-        <h1>Meet the Team</h1>
-        <div className="middlethis" dir='rtl'>
-          {produceImageWidgets()}
+  render () {
+    return (
+      <React.Fragment>
+        <div className= "about-container">
+          <h1>About</h1>
+          <p className="about">We endeavor to promote and improve the health of individuals in the
+          post acute setting through evidence based medical treatment and
+          prevention strategies, effective use of data,  advocacy, and
+          collaboration.
+  
+          Our goal is to offer consistent quality care that set the standards
+          in skilled nursing facility communities and exceed patients' expectations
+          while delivered in a caring, convenient, cost-effective and accessible
+          manner. This is exemplified through our belief of creating a culture
+          surrounding the core continuity of care for all of our patients,
+          creating lasting relationships with partners, and building strong
+          connections within the community.</p>
+          <h1>Meet the Team</h1>
+          <div className="middlethis" dir='rtl'>
+            {this.produceImageWidgets()}
+          </div>
+          <div>
+            {this.bio()}
+          </div>
         </div>
-        <div>
-          {bio(selected)}
-        </div>
-      </div>
-      <ContactUs />
-    </React.Fragment>
-  )
+        <ContactUs />
+      </React.Fragment>
+    )
+  }
 }
 
 export default About;
