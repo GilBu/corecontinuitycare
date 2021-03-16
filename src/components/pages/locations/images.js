69f4cab81beef240769e982b4174bff6eac1a6e4
gilbu <gilbertbuix57@gmail.com>
2021-03-16 16:47:20 -0700

    add san francisco health care location, change url to short url, & change padding for location

--- src/components/pages/locations/images.js
+++ src/components/pages/locations/images.js
@@ -5,7 +5,7 @@ export const imagesList= [
     address: '3400 Laguna Street, San Francisco, CA, 94123-2271',
     image: 'https://heritageonthemarina.org/wp-content/uploads/2019/04/heritage-home-header2.jpg',
     phone: "(415) 202-0300",
-    placeID: 'https://www.google.com/maps/place/?q=place_id:ChIJb7kbRNmAhYARiFq6Ogm-PVg'
+    placeID: 'https://goo.gl/maps/7EnCR254cDF5GZEx8'
   },
   {
     name: 'Victorian Manor',
@@ -13,7 +13,7 @@ export const imagesList= [
     image: 'https://www.senioradvice.com/img/providers/12906/victorian-manor-6.jpg',
     phone: "(415) 921-7550",
     fax: "(415) 921-1895",
-    placeID: 'https://www.google.com/maps/place/?q=place_id:ChIJZaQncbCAhYAR4nBuPEkA56Y'
+    placeID: 'https://goo.gl/maps/PJcvgcfUSHc1CUDC7'
   },
   {
     name: 'Pacific Heights Transitional Care Center',
@@ -21,7 +21,7 @@ export const imagesList= [
     image: 'https://s3-media4.fl.yelpcdn.com/bphoto/07fzt7w71diGduBo7Medyg/o.jpg',
     phone: "(415) 563-7600",
     fax: "(415) 299-8184",
-    placeID: 'https://www.google.com/maps/place/?q=place_id:ChIJNaLJgsmAhYARDoICJkg7B3M'
+    placeID: 'https://goo.gl/maps/bvEaNKaBcEmx1LEF8'
   },
   {
     name: 'The Avenues Transitional Care Center',
@@ -29,7 +29,7 @@ export const imagesList= [
     image: 'http://avenueshealth.com/wp-content/uploads/2018/06/shutterstock_597617204.jpg',
     phone: "(415) 661-8787",
     fax: "(415) 566-7154",
-    placeID: 'https://www.google.com/maps/place/?q=place_id:ChIJ6waVtY59j4AR1sMkscb091o'
+    placeID: 'https://goo.gl/maps/UYJWdx3b75gHat679'
   },
   {
     name: 'St. Francis Pavillion',
@@ -43,8 +43,16 @@ export const imagesList= [
     name: 'St. Francis Heights',
     address: '35 Escuela Dr, Daly City, CA 94015',
     image: 'https://s3-media0.fl.yelpcdn.com/bphoto/NJLhYaEj9LaoTaY0Lq_ETA/o.jpg',
-    phone: "(925)462-2400",
+    phone: "(925) 462-2400",
     fax: "(925) 621-4997",
     placeID: 'https://goo.gl/maps/gEUZKGnRcNUpVbKB8'
+  },
+  {
+    name: 'San Francisco Health Care and Rehab',
+    address: '1477 Grove Street, San Francsico, CA 94117',
+    image: 'https://sfhcr.com/wp-content/uploads/2017/02/Facility-Picture.jpg',
+    phone: "(415) 563-0565",
+    fax: "(415) 738-2398",
+    placeID: 'https://g.page/SFHCR?share'
   },
 ]
