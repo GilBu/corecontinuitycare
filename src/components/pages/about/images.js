17457e9806fc83a0c1c86b2fd5993272020ca563
gilbu <gilbertbuix57@gmail.com>
2021-03-16 16:31:23 -0700

    update philp & patel bio

--- src/components/pages/about/images.js
+++ src/components/pages/about/images.js
@@ -12,22 +12,13 @@ export const imagesList= [
     name: 'Miteshkumar Patel, CEO, MD',
     title: 'Physician at Core Continuity of Care, Inc.',
     image: mPatel,
-    bio: 'Miteshkumar Patel is a board certified family medicine physician. He graduated family medicine residency from Medical College of Georgia in 2016. Has experience working in outpatient primary clinic, hospital medicine, emergency department and urgent cares. Started working in Post acute care in 2017 and has developed a strong interest in improving care in the area. He is able to do bedside joint injections, iv access, and bedside ultrasound. '
+    bio: 'Miteshkumar Patel is a board-certified family medicine physician. He graduated family medicine residency from Medical College of Georgia in 2016. Has experience working in outpatient primary clinic, hospital medicine, emergency department and urgent cares. Started working in Post-acute care in 2017 and has developed a strong interest in improving care in the area. He can perform bedside joint injections, iv access, and bedside ultrasound.'
   },
   {
     name: 'Philip Adrian Andres, MSN, FNP-BC',
     title: 'Co-Vice President and Partner at Core Continuity of Care, Inc.',
     image: pAndres,
-    bio: "Philip Andres is a board certified Family Nurse Practitioner who " +
-    "completed his Master of Science Nursing Degree in 2011 at Holy Names "+
-    "University and Bachelor of Science Nursing Degree at San Francisco State "+
-    "University in 2009. Philip specializes in Post Acute Medicine since 2016. "+
-    "Philip had worked as a Nurse Practitioner at Seton Medical Center at the Acute"+
-    ", Post Acute and Gero Psych floors. He also works part time as a Hospice "+
-    "NP. Prior to working as a NP, Philip worked as a RN in various settings "+
-    "such as Critical Care Transport RN, ICU, ER, IR, PACU, Oncology, Telemetry "+
-    "and Med-Surg units. Philip is proficient in Tagalog and Ilocano. "+
-    "Philip enjoys traveling and loves to eat."
+    bio: "Philip Andres is a board-certified Family Nurse Practitioner who completed his Master of Science Nursing Degree in 2011 at Holy Names University and Bachelor of Science Nursing Degree at San Francisco State University in 2009. Philip specializes in Post-Acute Medicine since 2016. Philip had worked as a Nurse Practitioner at Seton Medical Center at the Acute, Post-Acute and Gero Psych floors. He also works part time as a Hospice NP. Prior to working as a NP, Philip worked as a RN in various settings such as Critical Care Transport RN, ICU, ER, IR, PACU, Oncology, Telemetry and Med-Surg units. Philip is proficient in Tagalog and Ilocano. Philip enjoys traveling and loves to eat."
   },
   {
     name: 'Ingrid Valerio',
@@ -42,18 +33,6 @@ export const imagesList= [
     bio: 'Michael Chang is our Director of Provider Services. Michael graduated with a degree in Health Administration and Management with the University of Phoenix. He has had years of experience working within multiple facets in both the acute and post-acute setting. This allows him to be able to assess and prioritize the needs of both patients and providers, and raise service standards. Michael is also an integral part of the administrative operations and revenue cycle. He is passionate about providing support and improving quality of care for patients.'
   },
   {
-    name: 'Stephanie Kang, DO',
-    title: 'Physician at Core Continuity of Care, Inc.',
-    image: sKang,
-    bio: "Dr. Kang was born in the Bay Area but grew up on the East Coast where she attended Vassar College and the University of Michigan for her undergraduate studies. She attended medical school at Michigan State University College of Osteopathic Medicine and completed her residency at Genesys Regional Medical Center where she focused her education primarily on hospital medicine. She began her work as a hospitalist in Salinas, CA before moving up to the Bay Area where she continued providing inpatient care at a few hospitals in and around San Francisco. For the past few years, she has worked primarily as a nocturnist in the East Bay before joining Core Continuity of Care to shift her focus on providing post-acute care services."
-  },
-  {
-    name: 'Rachel Allison',
-    title: 'Clinical Administrative Analyst at Core Continuity of Care, Inc.',
-    image: rAllison,
-    bio: 'Rachel Allison is an aspiring physician. She graduated at University of California, Riverside in Neuroscience. She is currently applying for her Master\'s in Biomedical Sciences before going onto medical school.She believes that public health is an integral aspect of holistic, quality patient care, and looks forward to a career in which she can also address socioeconomic health disparities in underserved communities.She currently serves as a Clinic Operations Chair volunteer at Mabuhay Health Center, a student- run free community health clinic targeting underserved Filipino - American residents in San Francisco’s South of Market(SOMA) district.In her free time, she enjoys reading, playing the ukelele, and tending to her houseplants.'
-  },
-  {
     name: 'Polian Mac',
     title: 'Clinical Administrative Analyst at Core Continuity of Care, Inc.',
     image: pMac,
@@ -64,5 +43,17 @@ export const imagesList= [
     title: 'Nurse Practitioner at Core Continuity of Care',
     image: jWoo,
     bio: ''
+  },
+  {
+    name: 'Rachel Allison',
+    title: 'Clinical Administrative Analyst at Core Continuity of Care, Inc.',
+    image: rAllison,
+    bio: 'Rachel Allison is an aspiring physician. She graduated at University of California, Riverside in Neuroscience. She is currently applying for her Master\'s in Biomedical Sciences before going onto medical school.She believes that public health is an integral aspect of holistic, quality patient care, and looks forward to a career in which she can also address socioeconomic health disparities in underserved communities.She currently serves as a Clinic Operations Chair volunteer at Mabuhay Health Center, a student- run free community health clinic targeting underserved Filipino - American residents in San Francisco’s South of Market(SOMA) district.In her free time, she enjoys reading, playing the ukelele, and tending to her houseplants.'
+  },
+  {
+    name: 'Stephanie Kang, DO',
+    title: 'Physician at Core Continuity of Care, Inc.',
+    image: sKang,
+    bio: "Dr. Kang was born in the Bay Area but grew up on the East Coast where she attended Vassar College and the University of Michigan for her undergraduate studies. She attended medical school at Michigan State University College of Osteopathic Medicine and completed her residency at Genesys Regional Medical Center where she focused her education primarily on hospital medicine. She began her work as a hospitalist in Salinas, CA before moving up to the Bay Area where she continued providing inpatient care at a few hospitals in and around San Francisco. For the past few years, she has worked primarily as a nocturnist in the East Bay before joining Core Continuity of Care to shift her focus on providing post-acute care services."
   },
 ]
